5d5ad6bffe26cb5910ac345fb013b99e59284ef5
Kitty Gu <kexinkittygu@gmail.com>
2021-05-13 20:54:26 +0100

    Multi-thread scene transition collection

--- scene_transition_collector.py
+++ scene_transition_collector.py
@@ -1,4 +1,5 @@
 import json
+import multiprocessing as mp
 import time
 
 import numpy as np
@@ -10,23 +11,38 @@ start_time = time.time()
 
 npz_file_path = './transition_collection_hist.npz'
 
+pool = mp.Pool(mp.cpu_count())
+
 with open(TVQA_PLUS_TRAIN_JSON) as f:
     all_json_data = json.load(f)
 
+all_hist_list = [
+    pool.apply(
+        atd.gen_pixel_diff,
+        args=(all_json_data[i], False, 'hist-all')
+    )['pixel_diff_score']
+    for i in range(15)
+]
+
 all_hist_all_diff_score = []
-all_hist_reg_diff_score = []
-for i in range(len(all_json_data)):
-    data = all_json_data[i]
-    all_hist_all_diff_score += \
-        atd.gen_pixel_diff(data, score_method='hist-all')['pixel_diff_score']
-    all_hist_reg_diff_score += \
-        atd.gen_pixel_diff(data, score_method='hist-reg')['pixel_diff_score']
+for l in all_hist_list:
+    all_hist_all_diff_score += l
+
+# all_hist_all_diff_score = []
+# all_hist_reg_diff_score = []
+# for i in range(15):
+#     data = all_json_data[i]
+#     all_hist_all_diff_score += \
+#         atd.gen_pixel_diff(data, score_method='hist-all')['pixel_diff_score']
+#     all_hist_reg_diff_score += \
+#         atd.gen_pixel_diff(data, score_method='hist-reg')['pixel_diff_score']
 
 print(F'Time:                {time.time() - start_time}')
 print(F'Len hist-all scores: {len(all_hist_all_diff_score)}')
-print(F'Len hist-reg scores: {len(all_hist_reg_diff_score)}')
+# print(F'Len hist-reg scores: {len(all_hist_reg_diff_score)}')
 
 np.savez_compressed(npz_file_path,
-                    hist_all_diff_scores=np.array(all_hist_all_diff_score),
-                    hist_reg_diff_scores=np.array(all_hist_reg_diff_score))
-
+                    hist_all_diff_scores=np.array(all_hist_all_diff_score)
+                    # ,
+                    # hist_reg_diff_scores=np.array(all_hist_reg_diff_score)
+                    )
